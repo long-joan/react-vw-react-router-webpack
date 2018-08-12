2fefab3c0fb4813b9a3e80e4f9c8fa0718f98eef
long-joan <18618281002@163.com>
2018-08-12 22:17:04 +0800

    新增按需加载

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component }from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './js/home';
-import Children from './js/children';
-
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import Loadable from 'react-loadable';
 import './css/App.css';
-export default class App extends Component {
-	render(){
-		return (
-			<Router>
-				<Switch>
-					<Route exact path="/home" component={Home} ></Route>
-					<Route path="/children" component={Children}></Route>
-				</Switch>
-			</Router>
-		)
-	}
-}
+const Loading = () => <div>Loading...</div>;
+
+const Home = Loadable({
+  loader: () => import('./js/home'),
+  loading: Loading,
+});
+
+const Children = Loadable({
+  loader: () => import('./js/children'),
+  loading: Loading,
+});
+
+export const App = () => (
+	<Router>
+		<Switch>
+			<Route exact path="/home" component={Home} ></Route>
+			<Route path="/children" component={Children}></Route>
+			<Redirect to='home'/>
+		</Switch>
+	</Router>
+  );
