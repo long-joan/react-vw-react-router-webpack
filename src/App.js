1488e3b283779e0079bf439624767b0fa76b5b73
long-joan <18618281002@163.com>
2018-08-12 22:41:21 +0800

    add loading

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component }from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Loadable from 'react-loadable';
+import Loading from './component/Loading';
 import './css/App.css';
-const Loading = () => <div>Loading...</div>;
+const Loading_dom = () => <Loading/>;
 
 const Home = Loadable({
   loader: () => import('./js/home'),
-  loading: Loading,
+  loading: Loading_dom,
 });
 
 const Children = Loadable({
   loader: () => import('./js/children'),
-  loading: Loading,
+  loading: Loading_dom,
 });
 
 export const App = () => (
